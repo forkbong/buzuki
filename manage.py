000123a73ea2298d1ba412b6d96f1e91c422ed51
Panagiotis Ktistakis <panktist@gmail.com>
2016-09-15 01:43:16 +0300

    manage.py: add gunicorn command

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+from multiprocessing import cpu_count
 
 import click
 from flask import current_app
 from flask.cli import FlaskGroup
+from gunicorn.app.base import BaseApplication
 
 from buzuki import create_app, db
 from buzuki.models import Song
@@ -42,6 +44,26 @@ def import_songs(directory):
         song = Song(name=name, artist=artist, link=link, body=body.strip('\n'))
         db.session.add(song)
     db.session.commit()
+
+
+@cli.command()
+@click.option('-w', '--workers', help="Number of workers.",
+              default=2 * cpu_count() + 1)
+@click.option('-h', '--host', help="The interface to bind to.",
+              default='127.0.0.1')
+@click.option('-p', '--port', help="The port to bind to.", default=8000)
+def gunicorn(workers, host, port):
+    """Run a production server with gunicorn."""
+
+    class GunicornApplication(BaseApplication):
+        def load_config(self):
+            self.cfg.set('bind', f'{host}:{port}')
+            self.cfg.set('workers', workers)
+
+        def load(self):
+            return create_app()
+
+    GunicornApplication().run()
 
 
 if __name__ == '__main__':
