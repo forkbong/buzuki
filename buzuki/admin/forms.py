4f7a4267db8c90e36f2d78dedd850e64f37ff184
Panagiotis Ktistakis <panktist@gmail.com>
2019-02-10 16:05:08 +0200

    Add URL validator to link field

--- buzuki/admin/forms.py
+++ buzuki/admin/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import URL, DataRequired
 
 
 class SongForm(FlaskForm):
@@ -9,7 +9,7 @@ class SongForm(FlaskForm):
     scale = TextAreaField('Scale', [DataRequired()])
     rhythm = TextAreaField('Rhythm', [DataRequired()])
     body = TextAreaField('Body', [DataRequired()])
-    link = StringField('YouTube link')
+    link = StringField('YouTube link', [DataRequired(), URL()])
 
 
 class PasswordForm(FlaskForm):
