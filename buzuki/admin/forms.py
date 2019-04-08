3ccc1c961ca24d89d9cefa01d167afe205b6ec1c
Panagiotis Ktistakis <panktist@gmail.com>
2019-04-08 21:05:15 +0300

    Translate more strings to Greek

--- buzuki/admin/forms.py
+++ buzuki/admin/forms.py
@@ -4,12 +4,25 @@ from wtforms.validators import URL, DataRequired
 
 
 class SongForm(FlaskForm):
-    name = StringField('Name', [DataRequired()])
-    artist = StringField('Artist', [DataRequired()])
-    scale = TextAreaField('Scale', [DataRequired()])
-    rhythm = TextAreaField('Rhythm', [DataRequired()])
-    body = TextAreaField('Body', [DataRequired()])
-    link = StringField('YouTube link', [DataRequired(), URL()])
+    name = StringField('Τίτλος', [DataRequired('Ο τίτλος είναι υποχρεωτικός')])
+    artist = StringField(
+        'Καλλιτέχνης', [DataRequired('Ο καλλιτέχνης είναι υποχρεωτικός')]
+    )
+    scale = TextAreaField(
+        'Δρόμος', [DataRequired('Ο δρόμος είναι υποχρεωτικός')]
+    )
+    rhythm = TextAreaField(
+        'Ρυθμός', [DataRequired('Ο ρυθμός είναι υποχρεωτικός')]
+    )
+    body = TextAreaField(
+        'Τραγούδι', [DataRequired('Το τραγούδι είναι υποχρεωτικό')]
+    )
+    link = StringField(
+        'YouTube link', [
+            DataRequired('Το link είναι υποχρεωτικό'),
+            URL(message='Αυτό δεν ήταν URL')
+        ]
+    )
 
 
 class PasswordForm(FlaskForm):
