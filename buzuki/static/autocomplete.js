ec78965fbfc5b7ee1a2aa990a3d7293b2a2d6b1a
Panagiotis Ktistakis <panktist@gmail.com>
2020-10-17 12:49:53 +0300

    Revert "Use elasticsearch again"
    This reverts commit 78407cb58e62e6bc5aa423dbaf2047c8062cf3ed.

--- buzuki/static/autocomplete.js
+++ buzuki/static/autocomplete.js
@@ -37,7 +37,7 @@
       return;
     }
     timeout = setTimeout(() => {
-      fetch(`/api/search/${query}/`)
+      fetch(`/api/autocomplete/?q=${query}`)
         .then((resp) => resp.json())
         .then((data) => {
           if (!data.length) {
